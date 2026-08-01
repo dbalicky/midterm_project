672709022794db13c47db934d0525d269ca701b5
dbalicky <dbalicky7@gmail.com>
2026-08-01 16:51:13 -0400

    updated exceptions.py

--- app/exceptions.py
+++ app/exceptions.py
@@ -14,3 +14,21 @@ class ValidationError(CalculatorError):
     value is entered, or when more arguments are given than allowed.
     """
     pass
+
+class OperationError(CalculatorError):
+    """
+    This exception is raised when a calculation operation fails.
+    
+    Indivcates failures during execution of arithmetic operations, such 
+    as dividing by zero or an invalid operation.
+    """
+    pass
+
+class ConfigurationError(CalculatorError):
+    """
+    This exception is raised when the configuration of the calculator is invalid.
+
+    Examples that raise this error include invalid directory paths or
+    faulty configuration values.
+    """
+    pass
